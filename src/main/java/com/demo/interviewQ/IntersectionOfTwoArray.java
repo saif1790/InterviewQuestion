941cc7d12515ad7f96147b5b1d62e23ca477f8af
saif.alam <saif.alam@10.60.0.206>
2023-02-11 17:07:49 +0530

    Added intersection of 2 array prog.

---
+++ src/main/java/com/demo/interviewQ/IntersectionOfTwoArray.java
@@ -0,0 +1,16 @@
+package com.demo.interviewQ;
+
+import java.util.Arrays;
+
+public class IntersectionOfTwoArray {
+
+	public static void main(String[] args) {
+
+		int a1[] = { 4,9,5 };
+
+		int a2[] = { 9,4,9,8,4 };
+
+		System.out.println("Intersection of 2 arrays");
+		Arrays.stream(a1).filter(x -> Arrays.stream(a2).anyMatch(y -> y == x)).distinct().forEach(System.out::println);
+	}
+}
